9844a3eb40028b9029bcfb4eb6739ce4733b2ba9
Celal Aygar <celal.aygar@gmail.com>
2018-07-15 23:39:22 +0300

    Update and rename TestSorting.java to Sorting_With_Collections.java

--- project2/Sorting_With_Collections.java
+++ project2/Sorting_With_Collections.java
@@ -10,20 +10,17 @@ public class TestSorting {
 	public static void main(String[] args) {
 
 		List<Developer> result = new ArrayList<Developer>();
-
 		result.add(new Developer("fatih", new BigDecimal("70000"), 33));
 		result.add(new Developer("celal", new BigDecimal("80000"), 20));
 		result.add(new Developer("ronaldo", new BigDecimal("100000"), 10));
 		result.add(new Developer("recep", new BigDecimal("20000"), 15));
 		result.add(new Developer("neymar", new BigDecimal("30000"), 25));
 		result.add(new Developer("acun", new BigDecimal("170000"), 55));
-		result.add(new Developer("sabancý", new BigDecimal("13000"), 85));
-		result.add(new Developer("koç", new BigDecimal("11000"), 45));
+		result.add(new Developer("sabancÃ½", new BigDecimal("13000"), 85));
+		result.add(new Developer("koÃ§", new BigDecimal("11000"), 45));
 		System.out.println("Before Sort ----------------------------");
-		int i =1;
 		for (Developer developer : result) {
 			System.out.println(i+" : "+developer.getName()+" "+developer.getAge()+" "+developer.getPrice()+" ");
-			i++;
 		}
 
 		// sort by age
@@ -33,7 +30,6 @@ public class TestSorting {
 				return o1.getAge() - o2.getAge();
 			}
 		});
-		i =1;
 		System.out.println("After Sort for age ----------------------------");
 		for (Developer developer : result) {
 			System.out.println(i+" : "+developer.getAge()+" "+developer.getName()+" "+developer.getPrice()+" ");
@@ -47,12 +43,9 @@ public class TestSorting {
 				return o1.getName().compareTo(o2.getName());
 			}
 		});
-		
-		i =1;
 		System.out.println("After Sort for name ----------------------------");
 		for (Developer developer : result) {
 			System.out.println(i+" : "+developer.getName()+" "+developer.getAge()+" "+developer.getPrice()+" ");
-			i++;
 		}
 		
 		//sort by salary
@@ -61,16 +54,10 @@ public class TestSorting {
 			public int compare(Developer o1, Developer o2) {
 				return o1.getPrice().compareTo(o2.getPrice());
 			}
-		});				
-
-		i =1;
+		});
 		System.out.println("After Sort for price ----------------------------");
 		for (Developer developer : result) {
 			System.out.println(i+" : "+developer.getPrice()+" "+developer.getName()+" "+developer.getAge()+" ");
-			i++;
 		}
-		
 	}
-
-
 }
