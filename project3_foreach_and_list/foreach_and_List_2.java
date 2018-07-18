ce99f00967c2fc87d2b6afa88f4eeb6efd1fed37
Celal Aygar <celal.aygar@gmail.com>
2018-07-18 19:58:09 +0300

    Update foreach_and_List_2.java

--- project3_foreach_and_list/foreach_and_List_2.java
+++ project3_foreach_and_list/foreach_and_List_2.java
@@ -21,14 +21,11 @@ public class foreach_and_List_2 {
 		}
 		System.out.println("\n"); 
 		
-		
 		items.forEach(item->System.out.print(item+" "));
-		System.out.println("\n"); 			//fatih bilal feride gül haydar tolga zeki 
-		
+		System.out.println("\n"); 		//fatih bilal feride gül haydar tolga zeki 
 		
 		items.forEach(System.out::print); 	//fatihbilalferidegülhaydartolgazeki
 		System.out.println("\n");
-		
 		
 		items.forEach(item->{
 			if("haydar".equals(item)){
@@ -36,19 +33,18 @@ public class foreach_and_List_2 {
 			}
 		});
 		
-		
 		List<String> alphaUpper = new ArrayList<>();
 		for (String s : items) {
 		    alphaUpper.add(s.toUpperCase());
 		}
 
-		System.out.println(items); 			//[fatih, bilal, feride, gül, haydar, tolga, zeki]
-		System.out.println(alphaUpper); 	//[FATÝH, BÝLAL, FERÝDE, GÜL, HAYDAR, TOLGA, ZEKÝ]
+		System.out.println(items); 		//[fatih, bilal, feride, gül, haydar, tolga, zeki]
+		System.out.println(alphaUpper); 	//[FATİH, BİLAL, FERİDE, GÜL, HAYDAR, TOLGA, ZEKİ]
 
 		// Java 8
 		List<String> collect = items.stream().map(String::toUpperCase).collect(Collectors.toList());
 
-		System.out.println(collect);		 //[FATÝH, BÝLAL, FERÝDE, GÜL, HAYDAR, TOLGA, ZEKÝ]
+		System.out.println(collect);		 //[FATİÝH, BİLAL, FERİDE, GÜL, HAYDAR, TOLGA, ZEKİ]
 
 	}
 
