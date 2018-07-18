36ad4e821ca24e36430270b6fa1529127ad10550
Celal Aygar <celal.aygar@gmail.com>
2018-07-18 20:07:19 +0300

    Update How_To_Use_List.java

--- project3_foreach_and_list/How_To_Use_List.java
+++ project3_foreach_and_list/How_To_Use_List.java
@@ -9,25 +9,25 @@ public class How_To_Use_List {
 	public static void main(String[] args) {
         
 		int[] number = {12, 21, 34, 46, 52, 61, 72, 86, 93, 10};
-        List<Integer> list1 = Arrays.stream(number).boxed().collect(Collectors.toList());
-        System.out.println("list : " + list1);  // output : list : [12, 21, 34, 46, 52, 61, 72, 86, 93, 10]
-        
-        String[] alphabet = new String[]{"ALİ", "CELAL", "DERYA", "ACUN", "MEHMET"};
+		List<Integer> list1 = Arrays.stream(number).boxed().collect(Collectors.toList());
+		System.out.println("list : " + list1);  // output : list : [12, 21, 34, 46, 52, 61, 72, 86, 93, 10]
 
-        // Convert String Array to List
-        List<String> list = Arrays.asList(alphabet);
+		String[] alphabet = new String[]{"ALİ", "CELAL", "DERYA", "ACUN", "MEHMET"};
 
-        if (list.contains("ALİ") || list.contains("BİLAL")) {
-            System.out.println("1. Hello ALİ or BİLAL");	// output : 1. Hello ALİ or BİLAL
-        }
+		// Convert String Array to List
+		List<String> list = Arrays.asList(alphabet);
 
-        if (list.containsAll(Arrays.asList("ALİ", "BİLAL"))) {
-            System.out.println("2. Hello ALİ or BİLAL");	// don't work
-        }
+		if (list.contains("ALİ") || list.contains("BİLAL")) {
+		    System.out.println("1. Hello ALİ or BİLAL");	// output : 1. Hello ALİ or BİLAL
+		}
 
-        if (list.containsAll(Arrays.asList("ALİ", "DERYA"))) {
-            System.out.println("3. Hello ALİ and DERYA");	// output : 3. Hello ALİ and DERYA
-        }
+		if (list.containsAll(Arrays.asList("ALİ", "BİLAL"))) {
+		    System.out.println("2. Hello ALİ or BİLAL");	// don't work
+		}
+
+		if (list.containsAll(Arrays.asList("ALİ", "DERYA"))) {
+		    System.out.println("3. Hello ALİ and DERYA");	// output : 3. Hello ALİ and DERYA
+		}
 
 	}
 
