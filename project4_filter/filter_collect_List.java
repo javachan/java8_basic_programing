b05f1395a9ed2e51a56513bc78f064b8ce903830
Celal Aygar <celal.aygar@gmail.com>
2018-08-19 01:14:57 +0300

    Update filter_collect_List.java

--- project4_filter/filter_collect_List.java
+++ project4_filter/filter_collect_List.java
@@ -8,13 +8,14 @@ public class filter_collect_List {
 
 	public static void main(String[] args) {
 
-        List<String> lines = Arrays.asList("spring", "core", "aop", "jpa", "hibernate");
+		List<String> lines = Arrays.asList("spring", "core", "aop", "jpa", "hibernate");
 
-        List<String> result = lines.stream()                // convert list to stream
-                .filter(line -> !"core".equals(line))     	// we dont like core
-                .collect(Collectors.toList());              // collect the output and convert streams to a List
+		List<String> result = lines.stream()		// convert list to stream
+			.filter(line -> !"core".equals(line))	// we dont like core
+			.collect(Collectors.toList());		// collect the output and convert streams to a List
 
-        result.forEach(System.out::println);                //output : spring, aop jpa hibernate
+		result.forEach(System.out::println);		//output : spring, aop jpa hibernate
+
 	}
 
 }
