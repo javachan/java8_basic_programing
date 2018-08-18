194998dcf9a9dc7965d57d1f3bc6198721f50b4e
Celal Aygar <celal.aygar@gmail.com>
2018-08-19 01:02:51 +0300

    Update Duration_1.java

--- project6_3_Period_ChronoUnit_and_Duration/Duration_1.java
+++ project6_3_Period_ChronoUnit_and_Duration/Duration_1.java
@@ -9,21 +9,21 @@ public class Duration_1 {
 
 	public static void main(String[] args) {
 
-        Duration oneHours = Duration.ofHours(1);
-        System.out.println(oneHours.getSeconds() + " seconds"); 	// output : 3600 seconds
+		Duration oneHours = Duration.ofHours(1);
+		System.out.println(oneHours.getSeconds() + " seconds"); 	// output : 3600 seconds
 
-        Duration oneHours2 = Duration.of(1, ChronoUnit.HOURS);
-        System.out.println(oneHours2.getSeconds() + " seconds");	// output : 3600 seconds
-        
-        LocalDateTime oldDate = LocalDateTime.of(2016, Month.AUGUST, 21, 05, 21, 53);
-        System.out.println(oldDate);								// output : 2016-08-21T05:21:53
-        
-        LocalDateTime newDate = LocalDateTime.of(2016, Month.NOVEMBER, 9, 10, 21, 56);
-        System.out.println(newDate);								// output : 2016-11-09T10:21:56
+		Duration oneHours2 = Duration.of(1, ChronoUnit.HOURS);
+		System.out.println(oneHours2.getSeconds() + " seconds");	// output : 3600 seconds
 
-        Duration duration = Duration.between(oldDate, newDate);
-        System.out.println(duration.getSeconds() + " seconds");		// output : 6930003 seconds
-		
+		LocalDateTime oldDate = LocalDateTime.of(2016, Month.AUGUST, 21, 05, 21, 53);
+		System.out.println(oldDate);					// output : 2016-08-21T05:21:53
+
+		LocalDateTime newDate = LocalDateTime.of(2016, Month.NOVEMBER, 9, 10, 21, 56);
+		System.out.println(newDate);					// output : 2016-11-09T10:21:56
+
+		Duration duration = Duration.between(oldDate, newDate);
+		System.out.println(duration.getSeconds() + " seconds");		// output : 6930003 seconds
+
 	}
 
 }
