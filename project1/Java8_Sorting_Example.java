8c15b0098ef1ed8b31c6bd1eeb31067d38064d93
Celal Aygar <celal.aygar@gmail.com>
2018-08-19 01:04:36 +0300

    Update Java8_Sorting_Example.java

--- project1/Java8_Sorting_Example.java
+++ project1/Java8_Sorting_Example.java
@@ -13,24 +13,21 @@ public class Java8_Sorting_Example {
 		List<String> items = Arrays.asList(
 				"apple", "apple", "melon", "apple",  "melon", "banana", "apple",
 				"melon", "orange", "orange","orange", "banana", "melon", "papaya", "melon", "lemon");
-        Map<String, Long> result =
-                items.stream().collect(
-                        Collectors.groupingBy(
-                                Function.identity(), Collectors.counting()
-                        )
-                );
-        System.out.println("-------------------------------------");
-        Map<String, Long> finalMap = new LinkedHashMap<>();
+		Map<String, Long> result =
+			items.stream().collect(
+				Collectors.groupingBy(
+					Function.identity(), Collectors.counting()
+				)
+			);
+		System.out.println("-------------------------------------");
+		Map<String, Long> finalMap = new LinkedHashMap<>();
 
-        //Sort a map and add to finalMap
-        result.entrySet().stream()
-                .sorted(Map.Entry.<String, Long>comparingByValue()
-                        .reversed()).forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
+		//Sort a map and add to finalMap
+		result.entrySet().stream()
+			.sorted(Map.Entry.<String, Long>comparingByValue()
+				.reversed()).forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
 
-        System.out.println(finalMap);
-		
-		
-		
-		
+		System.out.println(finalMap);
+
 	}
 }
