9b03bb0011d9fd1cf134d7426dbf58e2a859375a
Celal Aygar <celal.aygar@gmail.com>
2018-08-19 01:09:23 +0300

    Update foreach_and_List.java

--- project3_foreach_and_list/foreach_and_List.java
+++ project3_foreach_and_list/foreach_and_List.java
@@ -21,21 +21,25 @@ public class foreach_and_List {
 		}
 		System.out.println("\n");
 		
-		items.forEach(item->System.out.print(item+" ")); // output : 1 2 3 4 5 6 7 
+		// output : 1 2 3 4 5 6 7 
+		items.forEach(item->System.out.print(item+" ")); 
 		System.out.println("\n");
 		
-		items.forEach(System.out::print); 		 // output : 1234567
+		 // output : 1234567
+		items.forEach(System.out::print); 		
 		System.out.println("\n");
 		
+		// output : 3
 		items.forEach(item->{
 			if("3".equals(item)){
-				System.out.println(item); 	// output : 3
+				System.out.println(item); 	
 			}
 		});
 		
+		// output : list : [12, 21, 34, 46, 52, 61, 72, 86, 93, 10]
 		int[] number = {12, 21, 34, 46, 52, 61, 72, 86, 93, 10};
         	List<Integer> list = Arrays.stream(number).boxed().collect(Collectors.toList());
-       		System.out.println("list : " + list);  		// output : list : [12, 21, 34, 46, 52, 61, 72, 86, 93, 10]
+       		System.out.println("list : " + list);  		
         
 	}
 
